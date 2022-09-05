49b26e387350e3ed885104758349e59e23e7825b
Brikena Prendaj <bprendaj2@gmail.com>
2022-09-04 21:09:04 -0400

    update color blocks

--- portfolio_react/src/components/topbar/Topbar.jsx
+++ portfolio_react/src/components/topbar/Topbar.jsx
@@ -5,7 +5,10 @@ export default function Topbar() {
     <div className="topbar">
       <div className= "wrapper">
         <div className="left">
-          <a href="#intro" className="logo">bprendaj</a>
+          <a href="#intro" className="logo">brikena prendaj</a>
+          <div className="itemContainer">
+
+          </div>
         </div>
         <h1>hello</h1>
         <div className="right"></div>
