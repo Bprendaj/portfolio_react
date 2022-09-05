ca5ebbc42e285ce3cf210b206f201af95ebe98aa
Brikena Prendaj <bprendaj2@gmail.com>
2022-09-04 21:37:34 -0400

    add sidebar menu

--- portfolio_react/src/App.jsx
+++ portfolio_react/src/App.jsx
@@ -2,13 +2,15 @@ import Topbar from "./components/topbar/Topbar";
 import Intro from"./components/intro/Intro"
 import Portfolio from "./components/portfolio/Portfolio"
 import Works from "./components/works/Works"
-import Contact from "./components/contact/contact"
+import Contact from "./components/contact/Contact"
+import Menu from "./components/menu/Menu"
 import "./app.scss"
 
 function App() {
   return (
     <div className="app">
     <Topbar/>
+    <Menu/>
     <div className="sections">
       <Intro/>
       <Portfolio/>
