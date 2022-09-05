e7d5a9238d318d2e4f032e8dcce68c6e4de9bb21
Brikena Prendaj <bprendaj2@gmail.com>
2022-09-04 22:49:54 -0400

    add portfolio content

--- portfolio_react/src/components/portfolio/Portfolio.jsx
+++ portfolio_react/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,41 @@ import "./portfolio.scss"
 export default function portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/databaee.png" alt=""/>
+          <h3>Databae</h3>
+        </div>
+        <div className="item">
+          <img src="assets/databaee.png" alt=""/>
+          <h3>Databae</h3>
+        </div>
+        <div className="item">
+          <img src="assets/databaee.png" alt=""/>
+          <h3>Databae</h3>
+        </div>
+        <div className="item">
+          <img src="assets/databaee.png" alt=""/>
+          <h3>Databae</h3>
+        </div>
+        <div className="item">
+          <img src="assets/databaee.png" alt=""/>
+          <h3>Databae</h3>
+        </div>
+        <div className="item">
+          <img src="assets/databaee.png" alt=""/>
+          <h3>Databae</h3>
+        </div>
+      </div>
     </div>
   )
 }
